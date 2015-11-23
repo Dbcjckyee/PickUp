66e2e9cdda75380bb1ea4d0d7f68782d0b7e0b7a
Wellington Ginah <email": null>
2015-11-23 11:25:23 -0800

    adds participants to specific page. start on join ajax

---
+++ app/assets/javascripts/join_event/join_button.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//   $('body').on('submit', 'form .button_to', function(e) {
+//     e.preventDefault()
+//     var data = $(this).
+//     var url = $(this).attr('action')
+//   $.ajax({
+//         data: coords,
+//         method: "POST",
+//         url: '/events/map',
+//       }).done({
+
+//       })
+
+//   }
+//   })
+// }
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 	require 'textacular'
 	has_and_belongs_to_many :users
 
-  validates :event_name, :sport, :start, :location, presence: true
+  validates :event_name, :date, :sport, :start, :location, presence: true
 
   def creator
     p self
