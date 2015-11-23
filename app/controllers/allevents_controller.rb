c6edbefa0cc217a9d62326c937477a071a6b9c68
Justin <email": null>
2015-11-23 15:18:40 -0800

    refactor filtering for events

--- app/controllers/allevents_controller.rb
+++ app/controllers/allevents_controller.rb
@@ -11,24 +11,13 @@ class AlleventsController < ApplicationController
     elsif params[:key] == "location"
       @eventmatch = Event.current.near("#{params[:lat]} , #{params[:long]}", (params[:location].to_i), :order => "distance") #converts the params into a long/lat sequence and pulls
     elsif params[:key] == "date"
-      case params[:date]
-      when "week"
-        @eventmatch = Event.where(date: Date.today..1.week.from_now).order('date asc')
-      when "2weeks"
-        @eventmatch = Event.where(date: Date.today..2.weeks.from_now).order('date asc')
-      when "3weeks"
-        @eventmatch = Event.where(date: Date.today..3.weeks.from_now).order('date asc')
-      when "month"
-        @eventmatch = Event.where(date: Date.today..1.month.from_now).order('date asc')
-      end
+    @eventmatch = Event.where(date: eval(params[:date])).order('date asc')
     else
       @eventmatch = Event.current.order('event_name asc')
     end
-    p @eventmatch
     @eventmatch
     respond_to do |filter|
       filter.js
     end
   end
-
 end
