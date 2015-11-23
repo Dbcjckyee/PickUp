6de51cd225d723ca00e827b3fe9c5d07787d69c2
Robin Choi <robinchoii@gmail.com>
2015-11-23 11:46:23 -0800

    remove render welcome/index in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,11 +4,9 @@ before_action :current_user
     if current_user
       redirect_to events_path
     end
-    p request.location.city
+    request.location.city
 
     @events =Event.current.near("San Francisco").sample(3)
-
-    render 'welcome/index'
 
   end
 
