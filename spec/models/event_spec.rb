12e0b878eb9b428aeac0286ba2af20b385059688
Robin Choi <robinchoii@gmail.com>
2015-11-26 11:18:21 -0800

    add model event shoulda matchers rspec passing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do 
+	it {should validate_presence_of :event_name}
+	it {should validate_presence_of :sport}
+	it {should validate_presence_of :start}
+	it {should validate_presence_of :location}
+	it {should validate_presence_of :date}
+	it {should have_and_belong_to_many(:users)}
 end
