d10a57fc75120a4fbfca62095cc9cdc1b58879a1
dbcjckyee <dbcjckyee@gmail.com>
2016-01-04 14:56:58 -0800

    remove search/new.html because it is unused

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,6 @@ before_action :current_user
   def index
     p params[:search][:description]
     @events = Event.current.basic_search(params[:search][:description])
-
   end
 
   def search
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :current_user
+
 	def index
 	end
 
