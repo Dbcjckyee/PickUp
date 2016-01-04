f836df590b61ebf7874b946e3dbde3654e66102f
dbcjckyee <dbcjckyee@gmail.com>
2016-01-04 14:43:24 -0800

    clean up and refactor map.js for event show view

--- app/assets/javascripts/events/map.js
+++ app/assets/javascripts/events/map.js
@@ -1,22 +1,11 @@
 $(document).ready(function() {
-  function initMap() {
-    var mapOptions = {
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map2 = new google.maps.Map(document.getElementById("map2"), mapOptions);
-    return map2
-  }
-  function dropEventMarker(lati, lngi) {
-    var marker = new google.maps.Marker({
+  var map2 = new google.maps.Map(document.getElementById("map2"), {zoom:13});
+  initialLocation = new google.maps.LatLng($('#event-info').data('latInfo'), $('#event-info').data('lngInfo'));
+  map2.setCenter(initialLocation)
+  var marker = new google.maps.Marker({
     map: map2,
-    position: {lat: lati, lng: lngi},
-    label: "*"})
-  }
-  map2 = new initMap()
-  navigator.geolocation.getCurrentPosition(function (position) {
-    initialLocation = new google.maps.LatLng($('#event-info').data('latInfo'), $('#event-info').data('lngInfo'));
-    map2.setCenter(initialLocation)
-  });
-  dropEventMarker($('#event-info').data('latInfo'), $('#event-info').data('lngInfo'))
+    position: {lat: $('#event-info').data('latInfo'), lng: $('#event-info').data('lngInfo')},
+    label: "*"
+  })
+
 });
