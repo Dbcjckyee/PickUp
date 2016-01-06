fb76219c828d624e10444116474b242a1f08657d
dbcjckyee <dbcjckyee@gmail.com>
2016-01-06 14:01:48 -0800

    recompile assets

---
+++ public/assets/eventfilters/filtering-1a57d8e6a20e9b6b50007171c3c39ac6a77f94b0e735fe991565dd08c061ad4b.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  ajaxCall = function(args){
+    $('select').val(0);
+    $.ajax({
+      data: args,
+      method: "GET",
+      url: '/allevents/update'
+    })
+    .done(function(page){
+      $('#allevents').html(page['partial'])
+    })
+  }
+
+  $("#sport").change(function(){
+    var state = {
+      sport: $('select#sport :selected').val(),
+      key: "sport"
+    };
+    ajaxCall(state);
+  })
+
+  $("#location").change(function(){
+    success = function(position){
+      var state = {
+        location: $('select#location :selected').val(),
+        key: "location",
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      };
+      ajaxCall(state);
+    }
+    failure = function(position){
+      alert("You must share your position to find close events.")
+    }
+    navigator.geolocation.getCurrentPosition(success, failure)
+  })
+
+  $("#date").change(function(){
+    var state = {
+      date: $('select#date :selected').val(),
+      key: "date"
+    };
+    ajaxCall(state);
+  })
+
+  $("#clear").click(function(event){
+    event.preventDefault();
+    $('select').val(0);
+    var state = {
+      state: "neutral"
+    }
+     ajaxCall(state);
+  })
+
+})
---
+++ public/assets/events/mapsetup-114c3900facbe8e14770a730f2b99c9578c5e0392e9be2989f0d744225d9637c.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var map = new google.maps.Map(document.getElementById('mappp'), {zoom: 13});
+  var marker2 = new google.maps.Marker({map: map});
+
+  function changeMarkerPosition(tag, lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    tag.setPosition(latlng);
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var marker = new google.maps.Marker({
+      map: map,
+      position: {lat: position.coords.latitude, lng: position.coords.longitude},
+      label: "!"
+    });
+    map.setCenter(initialLocation)
+  });
+
+  $("a.showme").click(function(event){
+    event.preventDefault();
+    var lat = $(this).data('lat');
+    var long = $(this).data('long');
+    changeMarkerPosition(marker2, lat, long)
+    map.setZoom(16);
+    map.setCenter(new google.maps.LatLng(lat, long))
+  });
+
+});
