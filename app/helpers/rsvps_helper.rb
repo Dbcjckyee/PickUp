8df5874b611355986a6de0b08a34e6e3918ca47e
dbcjckyee <dbcjckyee@gmail.com>
2016-01-13 15:31:29 -0800

    fix bug that did not transfer ownership of events correctly when host leaves

--- app/helpers/rsvps_helper.rb
+++ app/helpers/rsvps_helper.rb
@@ -1,11 +1,12 @@
 module RsvpsHelper
   def creator_leave
     @event = Event.find(params[:id])
-    @new_host = @event.users.last
+    @event.users.destroy(User.find(current_user.id))
+    @new_host = @event.users.first
     @attendees = @event.users.size
-    if @attendees > 1
+    if @attendees >= 1
       @event.update_attribute(:creator_id, @new_host.id)
-      @event.users.destroy(User.find(current_user.id))
+      @event.save
     else
       @event.destroy
     end
