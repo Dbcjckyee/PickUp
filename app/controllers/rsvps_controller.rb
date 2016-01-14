a2f909c4cadb8c4b77a694bd586f9bdc8fcd340f
dbcjckyee <dbcjckyee@gmail.com>
2016-01-13 17:00:05 -0800

    add line in controller to prevent dupe rsvps for events

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -4,9 +4,8 @@ class RsvpsController < ApplicationController
 
   def create
     @event = Event.find(params[:id])
-    Rsvp.create(user_id: current_user.id, event_id: params[:id])
     unless @event.users.exists?(current_user)
-      @event.users << current_user
+      Rsvp.create(user_id: current_user.id, event_id: params[:id])
       flash[:notice] = "You have joined this event!"
       UserMailer.event_confirm_email(User.find(current_user.id), @event).deliver_now
       UserMailer.join_notification(@event.creator, @event).deliver_now
